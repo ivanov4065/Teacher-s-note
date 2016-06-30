3ac5f820519f5d028b431dd050f8c8b754452cd0
Roman Ivaniv <rivaniv@razomsolution.com>
2016-06-30 20:03:38 +0300

    Initial Commit

---
+++ TeacherBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TeacherBook
+//
+//  Created by Roman Ivaniv on 30.06.16.
+//  Copyright © 2016 Roman Ivaniv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TeacherBookTests/TeacherBookTests.m
@@ -0,0 +1,39 @@
+//
+//  TeacherBookTests.m
+//  TeacherBookTests
+//
+//  Created by Roman Ivaniv on 30.06.16.
+//  Copyright © 2016 Roman Ivaniv. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TeacherBookTests : XCTestCase
+
+@end
+
+@implementation TeacherBookTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
